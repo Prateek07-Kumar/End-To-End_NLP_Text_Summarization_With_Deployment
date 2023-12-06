1b1fb378b27e560bbc18e4a6a2365c6bdd399db2
Prateek Chaurasia <111336976+Prateek07-Kumar@users.noreply.github.com>
2023-12-06 23:10:56 +0530

    app.py_completed

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ from fastapi.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
 from fastapi.responses import Response
 from textSummarizer.pipeline.prediction import PredictionPipeline
+
+
+text:str = "What is Text Summarization?"
+
+app = FastAPI()
+
+@app.get("/", tags=["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
